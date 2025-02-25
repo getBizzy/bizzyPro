31fd99e5fb2c91afd591e5a510aa7eae45989b43
elizabeth koch <elk677@g.harvard.edu>
2025-02-25 13:38:49 -0500

    deploy

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,10 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("Environment Variables in Production:", process.env);
 console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
 console.log("Supabase Anon Key:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? "Loaded" : "Not Loaded");
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL?.trim();
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.trim();
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL and Anon Key are required.");
